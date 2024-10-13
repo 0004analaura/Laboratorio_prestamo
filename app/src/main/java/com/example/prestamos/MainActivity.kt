4dd97c865f58f1c2f3ca35d45c4c1af982751f37
Ana Laura Oliva <aolivac4@miumg.edu.gt>
2024-10-13 17:35:51 -0600

    Finalizado

--- app/src/main/java/com/example/prestamos/MainActivity.kt
+++ app/src/main/java/com/example/prestamos/MainActivity.kt
@@ -12,6 +12,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.prestamos.ui.theme.PrestamosTheme
+import com.example.prestamos.views.HomeView
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,28 +21,10 @@ class MainActivity : ComponentActivity() {
         setContent {
             PrestamosTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    HomeView()
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    PrestamosTheme {
-        Greeting("Android")
-    }
-}
